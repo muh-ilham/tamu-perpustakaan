cc0c0b3d27179281f80539853debd763790fd464
muh-ilham <adaillank@gmail.com>
2026-04-24 16:49:10 +0800

    Branding Update: Added dynamic Favicon based on app logo for enhanced professional identity across all pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @if(isset($global_settings['app_logo']) && $global_settings['app_logo'])
+            <link rel="icon" type="image/x-icon" href="{{ asset('storage/' . $global_settings['app_logo']) }}">
+        @endif
 
         <title>{{ $global_settings['app_name'] ?? config('app.name', 'Laravel') }}</title>
 
--- resources/views/visitor/index.blade.php
+++ resources/views/visitor/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @if(isset($global_settings['app_logo']) && $global_settings['app_logo'])
+        <link rel="icon" type="image/x-icon" href="{{ asset('storage/' . $global_settings['app_logo']) }}">
+    @endif
     <title>{{ $global_settings['app_name'] }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
